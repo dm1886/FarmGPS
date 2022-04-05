a65231ec0046f980133281971b2551ae74e32caf
Damiano Miazzi <damianomiazzi@gmail.com>
2022-04-05 19:42:20 +0800

    Initial Commit

---
+++ FarmGPS/FarmGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmGPSApp.swift
+//  FarmGPS
+//
+//  Created by Damiano Miazzi on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct FarmGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
